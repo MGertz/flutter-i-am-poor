b69b02c91f5654e81acfc2eacb4b0c7cfa13f509
Michael R. Gertz <michael@ringhus.dk>
2020-01-04 23:56:07 +0100

    added changes to final app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main()
+void main() {
+  runApp(
+    MaterialApp (
+      home: Scaffold(
+        backgroundColor: Colors.lightGreen,
+        appBar: AppBar(
+          title: Text("I Am Poor"),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Image(
+            image:
+              AssetImage('images/wood-charcoal-500x500-500x500.jpg')
+          ),
+        ),
+      ),
+    ),
+  );
+}
